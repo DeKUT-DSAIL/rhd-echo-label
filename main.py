0533146f5bd27760a82661736b5db29a784b7149
Lorna Mugambi <lornamugambik@gmail.com>
2021-05-23 17:26:20 +0300

    Added:Modifications

--- main.py
+++ main.py
@@ -16,8 +16,13 @@ from modules.imagelist import imagelist
 #image slider
 from modules.slideimages import slideimages
 
-list_of_checks = ['Correct', 'PLAX', 'NOT PLAX']
+list_of_validation = ['Correct', 'PLAX', 'NOT PLAX']
 
+list_of_conditions = ['Mitral Valve Regurgitation','Aortic Valve Regurgitation','Tricuspid Valve Regurgitation','Pulmonary Valve Regurgitation','Aortic Valve Stenosis','Mitral Valve Stenosis','Tricuspid Valve Stenosis','Pulmonary Valve Stenosis','Mitral Valve Prolapse']
+
+list_of_views = ['Parasternal long axis (PLAX)','Parasternal short axis(PSAX)','Apical Four Chamber(A4C)','Apical three chamber (A3C)','Apical two chamber(A2C)','Suprasternal(SSN)','Subcostal']
+
+list_of_severities = ['Mormal','Borderline rhd','Definite rhd']
     
 # }
 # external CSS stylesheets
@@ -32,24 +37,48 @@ app = dash_app.server
 x = 0 #the default image, my code has been built around the default being 0.#esp in single click.
 y = 0 #default1 when both clicked. #used when next_time_greater thanprev_timestamp
 z = 0 #default2 when both clicked. #used when prev_time_greater than next_timestamp
-imagenames = imagelist('./static/images')
+imagenames = imagelist('./assets/static/images')
 #num_images = len(imagenames)
 dash_app.title = "RHD Annotation Quality Control Check"
 
 df = pd.read_csv('RHD-Data - ValidationSet.csv')
 
 
-dash_app.layout = html.Div(style = {'textAlign' : 'center'},
-                        children = [html.H1('Quality Control Check'),
+dash_app.layout = html.Div([
+    html.Div([
+        html.H1("Quality Control Check"),
+        html.Img(src="/assets/favicon.ico"),
+    ], className="banner"),
+ html.Div(style = {'textAlign' : 'center'},
+                        children = [
                                     html.Div(html.Img(id = 'image-seen',src = imagenames[x],width = '700px',height = '500px',)),
 
                                         html.P(),
                                         html.Div(style = {'display': 'flex','align-items':'center','justify-content':'center'},
-                                        children = [html.P(children = [dcc.Dropdown(id = 'condition',
-                                                                        options = [ {'label' : i , 'value' : i} for i in list_of_checks], #list of checks
-                                                                        placeholder = "Select ",
+                                        children = [html.P(children = [dcc.Dropdown(id = 'validate',
+                                                                        options = [ {'label' : i , 'value' : i} for i in list_of_validation], #list of checks
+                                                                        placeholder = "Select validation status of current annotation ",
                                                                         multi = True,
                                                                         style={'height': '30px', 'width': '800px', 'textAlign' : "middle"}),
+
+                                                                        html.P(),
+                                                                        dcc.Dropdown( id = 'view',
+                                                                            options = [{'label' : j,'value': j } for j in list_of_views],#list of views
+                                                                            placeholder = "Select the view of the echocardiogram",
+                                                                            style={'height': '30px', 'width': '800px', 'textAlign' : 'center'}),
+
+                                                                        html.P(),
+                                                                        dcc.Dropdown(id = 'condition',
+                                                                        options = [ {'label' : i , 'value' : i} for i in list_of_conditions], #list of conditions
+                                                                        placeholder = "Select the condition or conditions",
+                                                                        multi = True,
+                                                                        style={'height': '30px', 'width': '800px', 'textAlign' : "middle"}),
+                                                            
+                                                                        html.P(),
+                                                                        dcc.Dropdown( id = 'severity',
+                                                                            options = [{'label' : j,'value': j } for j in list_of_severities],#list of severities
+                                                                            placeholder = "Select Severity of RHD",
+                                                                            style={'height': '30px', 'width': '800px', 'textAlign' : 'center'}),
                                         html.P(),
                                         html.P(children = [ html.Div(id = 'prevend'),html.Button('Prev',id = 'prevbutton'),html.Button('Next', id = 'nextbutton'),html.Div(id = 'nextend')],),
                                    
@@ -58,12 +87,13 @@ dash_app.layout = html.Div(style = {'textAlign' : 'center'},
                                     dash_table.DataTable(
                                                 id='table',
                                                 columns=[{"name": i, "id": i} for i in df.columns],
-                                                data=df.to_dict('records')
+                                                data=df.to_dict('records'),
+                                                page_size=10)
 
-                                    )
+                                   
                                                     ])])
                                     ])])
-
+])
 #Manage the image slider.
 @dash_app.callback(
     dash.dependencies.Output('image-seen','src'),
