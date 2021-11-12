5b289af313f437974fb477d11f5cf19916641031
Lorna Mugambi <lornamugambik@gmail.com>
2021-11-12 12:48:01 +0300

    removed file containing users details:

--- users.py
+++
@@ -1,7 +0,0 @@
-#Username password pairs(Private)
-VALID_USERNAME_PASSWORD_PAIRS = {
-    'Liesl Zuhlke': 'auth1',
-    'Ciira Maina': 'auth1',
-    'Lorna Mugambi': 'auth1',
-    'Guest': 'auth1'
-}
